de8e835e88d6b265da579a593829085731660880
SSSRAFA <71843015+SSSRAFA@users.noreply.github.com>
2026-07-11 14:01:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -33,8 +33,10 @@ app.get("/search", async (req, res) => {
 
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Servidor iniciado na porta 3000");
+app.listen(PORT, "0.0.0.0", () => {
+
+    console.log(`Servidor iniciado na porta ${PORT}`);
 
 });
